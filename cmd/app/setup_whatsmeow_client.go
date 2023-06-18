a16aa1da81ff352b2f863008c2392a9e3c5c7c80
defryheryanto <defry.heryanto@hotmail.com>
2023-06-19 02:37:38 +0700

    remove debug logger

--- cmd/app/setup_whatsmeow_client.go
+++ cmd/app/setup_whatsmeow_client.go
@@ -6,12 +6,10 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 	"go.mau.fi/whatsmeow"
 	"go.mau.fi/whatsmeow/store/sqlstore"
-	waLog "go.mau.fi/whatsmeow/util/log"
 )
 
 func setupWhatsmeowClient(sqlitePath string) (*whatsmeow.Client, error) {
-	dbLog := waLog.Stdout("Database", "DEBUG", true)
-	container, err := sqlstore.New("sqlite3", fmt.Sprintf("file:%s?_foreign_keys=on", sqlitePath), dbLog)
+	container, err := sqlstore.New("sqlite3", fmt.Sprintf("file:%s?_foreign_keys=on", sqlitePath), nil)
 	if err != nil {
 		return nil, fmt.Errorf("error initializing sqlite: %v", err)
 	}
@@ -19,7 +17,6 @@ func setupWhatsmeowClient(sqlitePath string) (*whatsmeow.Client, error) {
 	if err != nil {
 		return nil, fmt.Errorf("error getting device: %v", err)
 	}
-	clientLog := waLog.Stdout("Client", "DEBUG", true)
-	client := whatsmeow.NewClient(deviceStore, clientLog)
+	client := whatsmeow.NewClient(deviceStore, nil)
 	return client, nil
 }
