3a4804012d4de18cea84c1246d1c796a7410f2fd
defryheryanto <defry.heryanto@hotmail.com>
2023-06-29 01:09:23 +0700

    chore: add unit test

--- internal/assistant/event_handler_test.go
+++ internal/assistant/event_handler_test.go
@@ -4,7 +4,32 @@ import (
 	"testing"
 
 	"github.com/onsi/gomega"
+	whatsmeow_proto "go.mau.fi/whatsmeow/binary/proto"
+	"go.mau.fi/whatsmeow/types/events"
+	"google.golang.org/protobuf/proto"
 )
+
+func Test_getMessage(t *testing.T) {
+	m := gomega.NewWithT(t)
+	t.Run("should get message from conversation", func(t *testing.T) {
+		message := getMessage(&events.Message{
+			Message: &whatsmeow_proto.Message{
+				Conversation: proto.String("Hello World!"),
+			},
+		})
+		m.Expect(message).To(gomega.Equal("Hello World!"))
+	})
+	t.Run("should get message from extended text message if conversation is empty", func(t *testing.T) {
+		message := getMessage(&events.Message{
+			Message: &whatsmeow_proto.Message{
+				ExtendedTextMessage: &whatsmeow_proto.ExtendedTextMessage{
+					Text: proto.String("Hello World!!!"),
+				},
+			},
+		})
+		m.Expect(message).To(gomega.Equal("Hello World!!!"))
+	})
+}
 
 func Test_extractCommands(t *testing.T) {
 	m := gomega.NewWithT(t)
