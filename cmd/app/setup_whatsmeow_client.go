7227a5c8ddfce74554dfa02ea5c3aca2fc979f17
defrywiltvb <defry.heryanto@hotmail.com>
2025-06-23 21:29:31 +0700

    Set whatsapp device name as configurable

--- cmd/app/setup_whatsmeow_client.go
+++ cmd/app/setup_whatsmeow_client.go
@@ -4,12 +4,16 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/defryheryanto/whatsapp-assistant/config"
 	_ "github.com/mattn/go-sqlite3"
 	"go.mau.fi/whatsmeow"
+	"go.mau.fi/whatsmeow/store"
 	"go.mau.fi/whatsmeow/store/sqlstore"
+	"google.golang.org/protobuf/proto"
 )
 
 func setupWhatsmeowClient(ctx context.Context, sqlitePath string) (*whatsmeow.Client, error) {
+	store.DeviceProps.Os = proto.String(config.AppName)
 	container, err := sqlstore.New(ctx, "sqlite3", fmt.Sprintf("file:%s?_foreign_keys=on", sqlitePath), nil)
 	if err != nil {
 		return nil, fmt.Errorf("error initializing sqlite: %v", err)
--- config/config.go
+++ config/config.go
@@ -1,9 +1,11 @@
 package config
 
 var (
+	AppName                  string
 	DatabaseConnectionString string
 )
 
 func Init() {
+	AppName = getString("APP_NAME", "whatsapp-assistant-dev")
 	DatabaseConnectionString = getString("DATABASE_CONNECTION_STRING", "")
 }
